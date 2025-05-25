298e561ff8fd38dcfcdd7144b9b21892d0d0611a
AnjeshxShrestha <anjeshshrestha039@gmail.com>
2025-05-25 18:27:35 +0545

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 int main() {
     char op;
     double num1, num2;
 
     printf("Enter an operator (+, -, *, /): ");
     scanf("%c", &op);
-
     printf("Enter two numbers: ");
     scanf("%lf %lf", &num1, &num2);
 
