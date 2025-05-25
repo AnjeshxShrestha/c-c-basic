777b4414145ea2763492604d9342a4659b389cb7
AnjeshxShrestha <anjeshshrestha039@gmail.com>
2025-05-25 18:28:53 +0545

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 #include <string.h>
-
 int main() {
     char str[100];
     int i, vowels = 0;
-
     printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);
-
     for (i = 0; str[i] != '\0'; i++) {
         char ch = str[i];
         if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
